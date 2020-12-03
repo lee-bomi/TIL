432c58537e932b599d213b0f735dcb5b9683f98f
Emma baby <bomvll@naver.com>
2020-12-03 21:42:31 +0900

    20-1203 / Spring core Day2

---
+++ Spring/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.member.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void order(){
+        //given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        //when(주문을했을때)
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        //then(주문해서 할인금액확인)
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
