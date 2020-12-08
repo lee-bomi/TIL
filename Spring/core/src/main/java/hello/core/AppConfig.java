550fe8529017458102de18819081af0dfd092737
Emma baby <bomvll@naver.com>
2020-12-08 22:01:38 +0900

    20-1208 / Spring core Day4

--- Spring/core/src/main/java/hello/core/AppConfig.java
+++ Spring/core/src/main/java/hello/core/AppConfig.java
@@ -9,22 +9,26 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     //구현객체 생성 및 생성자통한 주입(연결)하는 역할
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
        // return new FixDiscountPolicy();
       return new RateDiscountPolicy();
