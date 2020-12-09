5b21fc367eaec15f62dcc5358b8c07eeb4f89fdb
Emma baby <bomvll@naver.com>
2020-12-09 23:25:33 +0900

    20-1209 / Spring core Day5

--- Spring/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ Spring/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
     //serviceImpl이 실행되면 생성자로 memberRepository인터페이스의 구현체가 들어온다
+    @Autowired //의존관계설정-Component와 셋뚜
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
---
+++ Spring/core/src/test/java/hello/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,11 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+public @interface MyIncludeComponent {
+
+
+}
